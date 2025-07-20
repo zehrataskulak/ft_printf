1d0120d2dceff5a7fbb1ac296eaeddc09700cafe
Zehra Taskulak <ztaskula@k1m14s07.42istanbul.com.tr>
2025-07-20 13:09:15 +0300

    fixed uninitialized variable

--- ft_put_hex.c
+++ ft_put_hex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_put_hex.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zzehra <zzehra@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ztaskula <ztaskula@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 05:04:20 by zzehra            #+#    #+#             */
-/*   Updated: 2025/07/10 05:04:21 by zzehra           ###   ########.fr       */
+/*   Updated: 2025/07/20 12:48:10 by ztaskula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_put_hex(unsigned long num, int isupper)
 	count = 1;
 	if (isupper == 0)
 		hex = "0123456789abcdef";
-	else if (isupper == 1)
+	else
 		hex = "0123456789ABCDEF";
 	if (num >= 16)
 		count += ft_put_hex(num / 16, isupper);
